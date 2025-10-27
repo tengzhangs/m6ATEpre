dab13b1c1f228a3e2063f274b67cbaf499ed486a
tengzhangs <147488840+tengzhangs@users.noreply.github.com>
2025-10-27 15:13:26 +0800

    Create predict_singlebase_m6Asites.sh

---
+++ code/predict_singlebase_m6Asites.sh
@@ -0,0 +1,3 @@
+conda activate R3.6
+cd ./sramp_simple/
+nohup perl runsramp.pl /home/disk3/zhangteng/m6A_translation/all_peak_seq/motif_peak_seq.fa /home/disk3/zhangteng/m6A_translation/all_peak_seq/singlebase_m6Asites.txt mature &
