b15552783b9c4dd434ea7c9c6f94f0495252e218
tengzhangs <147488840+tengzhangs@users.noreply.github.com>
2026-02-13 12:15:38 +0800

    Update DF1_motif_vec.R

--- code/DF1_motif_vec.R
+++ code/DF1_motif_vec.R
@@ -1,6 +1,6 @@
-load( "./proc_m6Asites_seqs.Rdata")
-pos_m6A_seq <- process_m6A_seq$pos_m6A_seq
-neg_m6A_seq <- process_m6A_seq$neg_m6A_seq
+#load( "./proc_m6Asites_seqs.Rdata")
+#pos_m6A_seq <- process_m6A_seq$pos_m6A_seq
+#neg_m6A_seq <- process_m6A_seq$neg_m6A_seq
 get_DF1_motif_infor <- function(peak_seq){
   DF1_motif <- c("GGAC","GAAC")
   DF1_loc <- vmatchPattern(DF1_motif[1], peak_seq)
@@ -19,10 +19,7 @@ get_DF1_motif_infor <- function(peak_seq){
   return(DF1motif_infor)
   
 }
-
-pos_m6A_DF1 <- get_DF1_motif_infor(peak_seq=pos_m6A_seq)
-neg_m6A_DF1 <- get_DF1_motif_infor(peak_seq=neg_m6A_seq)
-DF1_motif_vec <- function(DF1_start){
+DF1motif_vec <- function(DF1_start){
   DF1_motif_vec <- data.frame()
   for (i in 1:length(DF1_start)) {
     oneDF1vec <- vector(length = 501-4+1)
@@ -38,9 +35,15 @@ DF1_motif_vec <- function(DF1_start){
   colnames(DF1_motif_vec) <- NULL
   return(DF1_motif_vec)
 }
-pos_DF1motif_vec <- DF1_motif_vec(DF1_start=pos_m6A_DF1$DF1motif_start)
-neg_DF1motif_vec <- DF1_motif_vec(DF1_start=neg_m6A_DF1$DF1motif_start)
 
-write.csv(pos_DF1motif_vec,file = "./single_base_level/cdhit_proces/pos_motif_vec.csv",row.names = F)
+DF1_motif_vec <- function(pos_m6A_seq,neg_m6A_seq){
+  pos_m6A_DF1 <- get_DF1_motif_infor(peak_seq=pos_m6A_seq)
+  neg_m6A_DF1 <- get_DF1_motif_infor(peak_seq=neg_m6A_seq)
+  pos_DF1motif_vec <- DF1motif_vec(DF1_start=pos_m6A_DF1$DF1motif_start)
+  neg_DF1motif_vec <- DF1motif_vec(DF1_start=neg_m6A_DF1$DF1motif_start)
+  DF1_motif_vec_list <- list(pos_DF1motif_vec=pos_DF1motif_vec,neg_DF1motif_vec=neg_DF1motif_vec)
+  return(DF1_motif_vec_list)
+}
+#write.csv(pos_DF1motif_vec,file = "./single_base_level/cdhit_proces/pos_motif_vec.csv",row.names = F)
 
-write.csv(neg_DF1motif_vec,file = "./single_base_level/cdhit_proces/neg_motif_vec.csv",row.names = F)
+#write.csv(neg_DF1motif_vec,file = "./single_base_level/cdhit_proces/neg_motif_vec.csv",row.names = F)
